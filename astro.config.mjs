47da073ac358cb0fa71a6ff9c69d34e17e66c5c9
Wing Leung <wing@Wings-MacBook-Air.local>
2026-05-01 18:34:17 +0800

    Revert to b1f65

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://wingchunleung.github.io',
-  base: '/website',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
